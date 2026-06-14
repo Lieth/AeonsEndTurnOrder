49f1d07e9a296097842870f820c9163ee403f154
Jimmy Byström <jimmy.bystrom@techseed.se>
2026-06-14 18:14:09 +0200

    Added reorder of drawpile

--- Start-DrawPhaser.ps1
+++ Start-DrawPhaser.ps1
@@ -1,9 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$ImageName = "drawphaser"
-$ContainerName = "DrawPhaser"
 $HostPort = 8123
-$ContainerPort = 8123
 $Url = "http://127.0.0.1:$HostPort/"
 
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
@@ -12,25 +9,18 @@ if (-not (Get-Command docker -ErrorAction SilentlyContinue)) {
   Write-Error "Docker CLI was not found. Start Docker Desktop and try again."
 }
 
-$oldContainer = docker ps -aq --filter "name=^/$ContainerName$"
-if ($oldContainer) {
-  docker rm -f $ContainerName | Out-Null
+Write-Host "Starting DrawPhaser with docker compose ..."
+Push-Location $ScriptDir
+
+try {
+  docker compose up -d
+
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "docker compose failed to start DrawPhaser."
+  }
 }
-
-$imageExists = docker image ls --format "{{.Repository}}:{{.Tag}}" | Where-Object { $_ -eq "${ImageName}:latest" }
-if (-not $imageExists) {
-  Write-Host "Building image $ImageName ..."
-  docker build -t $ImageName "$ScriptDir"
-}
-
-Write-Host "Starting container $ContainerName ..."
-docker run -d --name $ContainerName -p "$HostPort`:$ContainerPort" $ImageName | Out-Null
-
-if ($LASTEXITCODE -ne 0) {
-  Write-Warning "Could not start container '$ContainerName'. Port $HostPort may already be in use."
-  Write-Host "Opening $Url in case another local instance is already running..."
-  Start-Process $Url
-  exit 0
+finally {
+  Pop-Location
 }
 
 Start-Sleep -Milliseconds 700
